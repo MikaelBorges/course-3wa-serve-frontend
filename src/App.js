a37917c068fd617bdc781ee036ba2a986c622c70
Mikael B <mika4ever@hotmail.fr>
2022-08-06 01:49:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import {useState, useEffect} from "react"
+import {loadAds} from './api/ads'
+
+function App() {
+  const [ads, setAds] = useState([])
+
+  useEffect(()=>{
+      loadAds()
+      .then((res)=>{
+          console.log('res', res) 
+          setAds(res)
+      })
+      .catch(err=>console.log(err))
+  },[])
+
+  return (
+    <section className="App">
+      <header className="App-header">
+      {ads.length > 0 && <ul>
+                {ads.map((ad)=>{
+                    return (
+                        <li key={ad._id}>
+                            {ad.name}
+                        </li>
+                    )
+                })}
+            </ul>}
+      </header>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+//on mets les informations, dans un fichier de config
+export const config = {
+    api_url: "https://mikaelborges-ads.herokuapp.com",
+    pict_url: "https://mikaelborges-ads.herokuapp.com/images/"
+}
