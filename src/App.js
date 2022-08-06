e59a88fcfb48be5876c30630572add01caab9e65
Mikael B <mika4ever@hotmail.fr>
2022-08-06 02:57:43 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {loadAds} from './api/ads'
 
 function App() {
   const [ads, setAds] = useState([])
+  const [isLogged, setIsLogged] = useState([false])
+  const [isAdmin, setIsAdmin] = useState([false])
 
   useEffect(()=>{
       loadAds()
@@ -15,20 +17,45 @@ function App() {
   },[])
 
   return (
-    <section className="annonces">
-      <h2>Toutes les annonces</h2>
-          {ads.length > 0 && <ul>
-              {ads.map((ad)=>{
-                  return (
-                      <article key={ad._id}>
-                          <h3>{ad.name}</h3>
-                          <p>{ad.description}</p>
-                          <p>{ad.price}</p>
-                      </article>
-                  )
-              })}
-          </ul>}
-    </section>
+    <>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          {!isLogged && 
+            <>
+              <li><a href="/user/register">S'inscrire</a></li>
+              <li><a href="/user/login">Connexion</a></li>
+            </>
+          }
+
+          {isLogged && 
+            <>
+              <li><a href="/profil">Profil</a></li>
+              <li><a href="/user/logout">Logout</a></li>
+              {isLogged && isAdmin &&
+                  <li><a href="/admin">Administration</a></li>
+              }
+            </>
+          }
+        </ul>
+      </nav>
+      <section className="annonces">
+        <h2>Toutes les annonces</h2>
+        {ads.length > 0 &&
+          <ul>
+            {ads.map((ad)=>{
+              return (
+                <li key={ad._id}>
+                  <h3>{ad.name}</h3>
+                  <p>{ad.description}</p>
+                  <p>{ad.price}</p>
+                </li>
+              )
+            })}
+          </ul>
+        }
+      </section>
+    </>
   );
 }
 
