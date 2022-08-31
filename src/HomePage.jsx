e1af3784cf4fe740d3789ccf84ce5bc1b94d7d78
Mikael B <mika4ever@hotmail.fr>
2022-08-31 11:26:51 +0200

    améliorations du full vertical mode

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,12 @@
 import { loadAds } from './api/ads'
 import { useState, useEffect } from 'react'
-import { heartIcon, starIcon, crownIcon, lightIcon, darkIcon, systemIcon, paperPencilIcon, leftHandIcon, rightHandIcon } from './icons/Icons'
+import {
+  lightIcon,
+  darkIcon,
+  systemIcon,
+  leftHandIcon,
+  rightHandIcon
+} from './icons/Icons'
 import styleOf from './HomePage.module.scss'
 import Card from './components/Card'
 
@@ -9,7 +15,7 @@ function HomePage(props) {
         [users, setUsers] = useState([]),
         [oddAds, setoddAds] = useState([]),
         [evenAds, setevenAds] = useState([]),
-        [horizontalCard, setHorizontalCard] = useState(true),
+        [horizontalCard, setHorizontalCard] = useState(false),
         [areAdsArranged, setAreAdsArranged] = useState(false),
         [layoutOneColumn, setLayoutOneColumn] = useState(false),
 
@@ -63,8 +69,8 @@ function HomePage(props) {
       <section className='pt-28 pb-24 dark:bg-slate-900 bg-white flex flex-col space-y-12 px-6'>
         <article
           className={`
-            mt-px
             flex
+            mt-px
             ${layoutOneColumn ? 'flex-col' : 'justify-between'}
         `}>
           <ul className={`${layoutOneColumn ? 'w-auto' : styleOf.adsColumnContainer}`}>
@@ -197,14 +203,22 @@ function HomePage(props) {
             </li>
             {oddAds.map(ad => {
               return (
-                <Card ad={ad} layoutOneColumn={layoutOneColumn} horizontalCard={horizontalCard} />
+                <Card
+                  ad={ad}
+                  horizontalCard={horizontalCard}
+                  layoutOneColumn={layoutOneColumn}
+                />
               )
             })}
           </ul>
           <ul className={`${layoutOneColumn ? 'w-auto' : styleOf.adsColumnContainer}`}>
             {evenAds.map(ad => {
               return (
-                <Card ad={ad} layoutOneColumn={layoutOneColumn} horizontalCard={horizontalCard} />
+                <Card
+                  ad={ad}
+                  horizontalCard={horizontalCard}
+                  layoutOneColumn={layoutOneColumn}
+                />
               )
             })}
           </ul>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,18 @@ import styleOf from './Card.module.scss'
 import { heartIcon, starIcon, crownIcon, paperPencilIcon } from '../icons/Icons'
 
 function Card(props) {
-
   const displayStars = (starsNb) => {
-        let stringOfStars = ''
-        while(starsNb) {
-          stringOfStars += starIcon
-          --starsNb
-        }
-        return stringOfStars
-      },
+          let stringOfStars = ''
+          while(starsNb) {
+            stringOfStars += starIcon
+            --starsNb
+          }
+          return stringOfStars
+        },
 
-      handleAddToFavorites = e => {
-        console.log('ajouter aux favoris')
-      }
+        handleAddToFavorites = e => {
+          console.log('ajouter aux favoris')
+        }
 
   return (
     <li
@@ -23,95 +22,149 @@ function Card(props) {
         mb-6
         rounded-3xl
         overflow-hidden
-        ${props.horizontalCard ? '' : 'flex-col'}
         ${props.layoutOneColumn ? 'flex' : ''}
-        ${props.layoutOneColumn && props.horizontalCard ? 'h-56' : ''}
+        ${props.horizontalCard ? '' : 'flex-col'}
+        ${props.layoutOneColumn && props.horizontalCard ?
+          styleOf.horizontalCard : ''
+        }
       `}
     >
       <div
         className={`
-          ${props.layoutOneColumn && props.horizontalCard ? styleOf.imagePartHorizontalAd : ''}
           flex
-          justify-center
           items-center
+          justify-center
+          ${props.layoutOneColumn && props.horizontalCard ?
+            styleOf.imagePartHorizontalAd : ''
+          }
         `}
       >
-        <img src={props.ad.imageUser} className={`${props.layoutOneColumn ? 'max-w-none h-full' : ''}`} />
+        <img
+          src={props.ad.imageUser}
+          className={`${props.layoutOneColumn ? 'max-w-none h-full' : ''}`}
+        />
       </div>
       <div
         className={`
-          ${props.layoutOneColumn && props.horizontalCard ? styleOf.textPartHorizontalAd : 'h-72'}
+          p-3
           flex
           flex-col
-          justify-between
           relative
-          p-3
           bg-slate-200
-          dark:bg-slate-700
+          justify-between
           dark:text-white
+          dark:bg-slate-700
+          ${props.layoutOneColumn && !props.horizontalCard ?
+            'text-2xl p-5 h-96' : ''
+          }
+          ${props.layoutOneColumn && props.horizontalCard ?
+            styleOf.textPartHorizontalAd : 'h-72'
+          }
         `}
       >
         {props.ad.superUser && (
           <span className={styleOf.superUserBadge}>{crownIcon}</span>
         )}
-
         <div>
           <h3
             className={`
-              ${styleOf.limitText}
-              ${styleOf.titleUser}
               font-bold
               leading-5
+              ${styleOf.limitText}
+              ${styleOf.titleUser}
+              ${props.layoutOneColumn && !props.horizontalCard ?
+                'leading-7' : ''
+              }
             `}
           >
             {props.ad.title}
           </h3>
-          <h4 className='text-sm text-ellipsis whitespace-nowrap overflow-hidden'>{props.ad.firstname} {props.ad.lastname}</h4>
+          <h4
+            className={`
+              text-ellipsis
+              overflow-hidden
+              whitespace-nowrap
+              ${props.layoutOneColumn && !props.horizontalCard ?
+                'text-lg mt-1' : 'text-sm'
+              }
+            `}
+          >
+            {props.ad.firstname} {props.ad.lastname}
+          </h4>
           <span>{displayStars(props.ad.starsNb)}</span>
         </div>
-
         <p className={`${styleOf.limitText} ${styleOf.descriptionUser}`}>{props.ad.description}</p>
-
-        <p className='text-fuchsia-800 text-sm dark:text-yellow-100'>{props.ad.price} / h</p>
-
+        <p
+          className={`
+            text-fuchsia-800
+            dark:text-yellow-100
+            ${props.layoutOneColumn && !props.horizontalCard ?
+              'text-lg' : 'text-sm'
+            }
+          `}
+        >
+          {props.ad.price} / h
+        </p>
         <div className='flex justify-between'>
           <button
             className={`
-              text-xl
-              bg-gray-100
               px-3
               py-2
               flex
+              text-xl
               items-center
               rounded-full
+              bg-gray-100
               dark:bg-slate-600
             `}
             onClick={e => handleAddToFavorites(e)}
           >
             <div className='text-xs'>{paperPencilIcon}</div>
-            {props.ad.reviewsNb > 0 && <div className='text-gray-400 text-base ml-2'>{props.ad.favoritesNb}</div>}
+            {props.ad.reviewsNb > 0 && (
+              <div
+                className={`
+                  ml-2
+                  text-gray-400
+                  ${props.layoutOneColumn && !props.horizontalCard ?
+                    'text-xl' : 'text-base'
+                  }
+                `}
+              >
+                {props.ad.favoritesNb}</div>
+            )}
           </button>
-
           <button
             className={`
-              text-xl
-              bg-gray-100
               px-2
               py-2
               flex
+              text-xl
               items-center
               rounded-full
+              bg-gray-100
               dark:bg-slate-600
             `}
             onClick={e => handleAddToFavorites(e)}
           >
             <div>{heartIcon}</div>
-            {props.ad.favoritesNb > 0 && <div className='text-red-600 text-base ml-1'>{props.ad.favoritesNb}</div>}
+            {props.ad.favoritesNb > 0 && (
+              <div
+                className={`
+                  ml-1
+                  text-red-600 
+                  ${props.layoutOneColumn && !props.horizontalCard ?
+                    'text-xl' : 'text-base'
+                  }
+                `}
+              >
+                {props.ad.favoritesNb}
+              </div>
+            )}
           </button>
         </div>
       </div>
     </li>
-  );
+  )
 }
 
 export default Card;
